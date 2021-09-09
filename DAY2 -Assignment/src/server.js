acfb010c607c0ab084616cedcf9415686bf2e26a
Kumar Rohit <rohit.kumar01091997@gmail.com>
2021-09-09 11:46:01 +0530

    multiple api calls using axios.all

--- DAY2 -Assignment/src/server.js
+++ DAY2 -Assignment/src/server.js
@@ -18,17 +18,75 @@ productStoreApp.get("/",(req,res)=>{
 })
 
 // URL mapped to return 10 products from API
+
+/***********Making a single async get call using axios**************/
+// productStoreApp.get("/getproducts",async (req,res)=>{
+
+//         try{
+//             let response = await axios.get('https://fakestoreapi.com/products');
+//             let result = response.data.slice(0,10)
+//             products = [...result]
+//             res.render("index.ejs",{data:products})
+//         } catch(error){
+//             console.error(error);
+//         }    
+// });
+
+/***********Making multiple async get calls using axios.all METHOD-1**************/
 productStoreApp.get("/getproducts",async (req,res)=>{
 
-        try{
-            let response = await axios.get('https://fakestoreapi.com/products');
-            let result = response.data.slice(0,10)
+    try{
+         // getting response array as responses and then assigning it to separate arrays
+        axios.all([
+            axios.get('https://fakestoreapi.com/products'),
+            axios.get('https://fakestoreapi.com/users')
+        ]).then(responses=>{
+            const productResponse = responses[0];
+            const userResponse = responses[1];
+            let result = productResponse.data.slice(0,10)
             products = [...result]
-            res.render("index.ejs",{data:products})
-        } catch(error){
-            console.error(error);
-        }    
+            res.render("index.ejs",{productData:products, userData:userResponse.data})
+        })   
+    } catch(error){
+        console.error(error);
+    }    
 });
+
+/***********Making multiple async get calls using axios.all METHOD-2**************/
+// productStoreApp.get("/getproducts",async (req,res)=>{
+
+//     try{
+//         //getting response array using axios.spread()
+//         axios.all([
+//             axios.get('https://fakestoreapi.com/products'),
+//             axios.get('https://fakestoreapi.com/users')
+//         ]).then(axios.spread((productResponse, userResponse)=>{
+//             let result = productResponse.data.slice(0,10)
+//             products = [...result]
+//             res.render("index.ejs",{productData:products, userData:userResponse.data})
+//         }))   
+//     } catch(error){
+//         console.error(error);
+//     }    
+// });
+
+/***********Making multiple async get calls using axios.all METHOD-3**************/
+// productStoreApp.get("/getproducts",async (req,res)=>{
+
+//     try{
+//         // getting response array using destructuring
+//         let [productResponse, userResponse] = await axios.all([
+//             axios.get('https://fakestoreapi.com/products'),
+//             axios.get('https://fakestoreapi.com/users')
+//         ])
+//         let result = productResponse.data.slice(0,10)
+//         products = [...result]
+//         res.render("index.ejs",{productData:products, userData:userResponse.data})
+           
+//     } catch(error){
+//         console.error(error);
+//     }    
+// });
 
 // URL mapped to a form for adding a new product
 productStoreApp.get('/addproduct',async (req,res)=>{
@@ -56,7 +114,7 @@ productStoreApp.post('/postproduct',async (req,res)=>{
         let result = response.data.slice(0,10)
         products = [...result]  
         products.push(newProduct) 
-        res.render('index.ejs', {data: products})
+        res.render('index.ejs', {productData: products})
     }catch(error){
         console.error(error);
     };
