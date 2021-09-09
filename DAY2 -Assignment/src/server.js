44094f7fc659b27c051033e11904b29f2d2472bd
Kumar Rohit <rohit.kumar01091997@gmail.com>
2021-09-09 10:04:34 +0530

    day-2 assignment modified

--- DAY2 -Assignment/src/server.js
+++ DAY2 -Assignment/src/server.js
@@ -1,42 +1,43 @@
 const express = require('express');
 const axios = require('axios');
 const parser = require('body-parser');
-const app = express();
-app.use(parser.json())
-app.use(parser.urlencoded({extended:false}));
+const productStoreApp = express();
+productStoreApp.use(parser.json())
+productStoreApp.use(parser.urlencoded({extended:false}));
 
-app.set("view engine","ejs");
-app.set("views","./src/views")
+productStoreApp.set("view engine","ejs");
+productStoreApp.set("views","./src/views")
 
 // array to store products fetched from API
 var products=[];
 
 
 // homepage
-app.get("/",(req,res)=>{
+productStoreApp.get("/",(req,res)=>{
     res.sendFile(__dirname+"/views/home.html");
 })
 
 // URL mapped to return 10 products from API
-app.get("/getproducts",async (req,res)=>{
-        await axios.get('https://fakestoreapi.com/products')
-        .then(response => {
+productStoreApp.get("/getproducts",async (req,res)=>{
+
+        try{
+            let response = await axios.get('https://fakestoreapi.com/products');
             let result = response.data.slice(0,10)
             products = [...result]
-        })
-        .catch(error => {
-        console.error(error);
-        });
+        } catch(error){
+            console.error(error);
+        }
+
         res.render("index.ejs",{data:products})
 });
 
 // URL mapped to a form for adding a new product
-app.get('/addproduct',async (req,res)=>{
+productStoreApp.get('/addproduct',async (req,res)=>{
     res.render("product.ejs")
 })
 
 // URL mapped to return the fetched products from API along with the newly added product
-app.post('/postproduct',async (req,res)=>{
+productStoreApp.post('/postproduct',async (req,res)=>{
 
     let newProduct = {
         "id":parseInt(req.body.id),
@@ -51,17 +52,18 @@ app.post('/postproduct',async (req,res)=>{
         }
     }
 
-    await axios.get("https://fakestoreapi.com/products")
-    .then(response => {
+    try{
+        let response = await axios.get("https://fakestoreapi.com/products");
         let result = response.data.slice(0,10)
         products = [...result]  
         products.push(newProduct) 
-    }).catch(error => {
+    }catch(error){
         console.error(error);
-    });
+    };
+
     res.render('index.ejs', {data: products});
 
 });
 
 
-app.listen(5000,()=>{console.log("Server is up on 5000")})
+productStoreApp.listen(5000,()=>{console.log("Server is up on 5000")})
