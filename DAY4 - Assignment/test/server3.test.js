1e11ebeb58e93da092b63aafab3bb7b064ccba14
Kumar Rohit <rohit.kumar01091997@gmail.com>
2021-09-15 12:20:50 +0530

    jwt authentication example

---
+++ DAY4 - Assignment/test/server3.test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const expect = require('expect');
+const request = require('supertest');
+const app = require('../server3');
+
+describe('unit testing / route',()=>{
+
+    it('should return ok status when triggered',()=>{
+        return request(app).get('/').then(response=>{
+            assert.equal(response.status, 200);
+        })
+    })
+
+    it('should ')
+})
