17e4c792e95fa955714f1346fd7d045ea996c1d9
Kumar Rohit <rohit.kumar01091997@gmail.com>
2021-09-17 12:16:27 +0530

    day8 assignment

---
+++ DAY8 - Assignment/src/pages/home-page/home-page.component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './home-page.component.css'
+import Directory from "../../components/directory/directory.component.jsx";
+
+const HomePage = () => {
+    return (
+      <div className="home-page">
+        <Directory />
+      </div>
+    );
+}
+
+export default HomePage;
+
